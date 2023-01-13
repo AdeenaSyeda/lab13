b56c2a4e1a65996c65959e92d9a1d56a1f5be912
AdeenaSyeda <97324527+AdeenaSyeda@users.noreply.github.com>
2023-01-13 09:56:48 +0500

    Add files via upload

---
+++ EvenTest.java
@@ -0,0 +1,12 @@
+public class EvenTest{
+	public static void main(string[]args) {
+		Queue q= new Linkedlist();
+		for (int i=0;i<10;i++) {
+			q.add(i); }
+
+System.outprintln("Queue having elements" + q); }
+int top =q.peek();
+System.out.println("element of queue at peek:" + top);
+int size= q.size();
+System.out.println("Size of the queue is:"+size); }}
+		
