687bf9e6df5621c05b58fb37a0e38cbd00d92c9f
AdeenaSyeda <97324527+AdeenaSyeda@users.noreply.github.com>
2023-01-13 09:59:53 +0500

    Update EvenTest.java

--- EvenTest.java
+++ EvenTest.java
@@ -1,12 +1,12 @@
 public class EvenTest{
 	public static void main(string[]args) {
 		Queue q= new Linkedlist();
-		for (int i=0;i<10;i++) {
+		for (int i=0;i<5;i++) {
 			q.add(i); }
 
 System.outprintln("Queue having elements" + q); }
 int top =q.peek();
-System.out.println("element of queue at peek:" + top);
+System.out.println("element:" + top);
 int size= q.size();
 System.out.println("Size of the queue is:"+size); }}
 		
